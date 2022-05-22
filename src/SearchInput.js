b09bfcf3b884d8ed087a94110ca9510cb093e0aa
Denzel Washington <denzelwashingtonsantana@gmail.com>
2022-05-22 16:50:03 -0300

    Primeiro commit

---
+++ src/SearchInput.js
@@ -0,0 +1,20 @@
+import React, { Fragment, useState, useEffect } from 'react'
+import useDebounce from './useDebounce'
+
+const SearchInput = ({value,onChange}) => {
+   const[displayValue,setDisplayValue] = useState(value)
+   const debouncedChange = useDebounce(onChange,500)
+   function handleChange(e){
+       setDisplayValue(e.target.value)
+       debouncedChange (e.target.value)
+   }
+   return (
+    <input 
+    type="search"
+    value={displayValue} 
+    onChange={handleChange}
+    />
+   )
+}
+
+export default SearchInput
